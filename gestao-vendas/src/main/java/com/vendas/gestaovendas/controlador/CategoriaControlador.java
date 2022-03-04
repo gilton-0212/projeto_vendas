0f79661c3c0e1f8e9b7ab0b0d5bba37526c43532
gilton <gilton.carvvalho.gcs@gmail.com>
2022-03-03 22:03:04 -0300

    atividade feita referente a issue #5

--- gestao-vendas/src/main/java/com/vendas/gestaovendas/controlador/CategoriaControlador.java
+++ gestao-vendas/src/main/java/com/vendas/gestaovendas/controlador/CategoriaControlador.java
@@ -3,6 +3,8 @@ package com.vendas.gestaovendas.controlador;
 import java.util.List;
 import java.util.Optional;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,7 @@ import com.vendas.gestaovendas.entidades.Categoria;
 
 import javax.validation.Valid;
 
+@Api(tags = "Categoria")
 @RestController
 @RequestMapping("/categoria")
 
@@ -22,22 +25,27 @@ public class CategoriaControlador {
 	@Autowired
 	private CategoriaServico categoriaSevico;
 	
+	@ApiOperation(value = "listar")
 	@GetMapping("")
 	public List<Categoria> listarTodas(){
 		return categoriaSevico.listarTodas();
 	}
-	
+
+	@ApiOperation(value = "Listar por Codigo")
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable Long codigo){
 		Optional<Categoria> categoria = categoriaSevico.buscarPorCodigo(codigo);
 		return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
 
+	@ApiOperation(value = "Salvar")
 	@PostMapping
 	public ResponseEntity<Categoria> salvar(@Valid @RequestBody Categoria categoria){
 		Categoria categoriaSalva =  categoriaSevico.salvar(categoria);
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
+
+	@ApiOperation(value = "Atualizar")
 	@PutMapping("/{codigo}")
 	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo, @Valid @RequestBody Categoria categoria){
 		return ResponseEntity.ok(categoriaSevico.atualizar(codigo, categoria));
--- gestao-vendas/src/main/java/com/vendas/gestaovendas/excecao/GestaoVendasTratamentoExcecao.java
+++ gestao-vendas/src/main/java/com/vendas/gestaovendas/excecao/GestaoVendasTratamentoExcecao.java
@@ -1,5 +1,7 @@
 package com.vendas.gestaovendas.excecao;
 
+import org.hibernate.validator.constraints.Length;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,22 +9,34 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @ControllerAdvice
 public class GestaoVendasTratamentoExcecao extends ResponseEntityExceptionHandler {
 
     public static final String CONSTANT_VALIDATION_NOT_BLANK = "NotBlank";
+    public static final String CONSTANT_VALIDATION_LENGTH = "Length";
+
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
                                                                   HttpStatus status, WebRequest request) {
        List<Erro> erros = gerarListaDeErros(ex.getBindingResult());
        return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request){
+        String msgUsuario = "Recurso Buscado não encontrado";
+        String msgDesenvolvedor = ex.toString();
+        List<Erro> erros = Arrays.asList(new Erro(msgUsuario,msgDesenvolvedor));
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     private List<Erro> gerarListaDeErros(BindingResult bindingResult) {
@@ -39,6 +53,10 @@ public class GestaoVendasTratamentoExcecao extends ResponseEntityExceptionHandle
         if (fieldError.getCode().equals(CONSTANT_VALIDATION_NOT_BLANK)){
             return fieldError.getDefaultMessage().concat(" é obrigatorio");
         }
+        if (fieldError.getCode().equals(CONSTANT_VALIDATION_LENGTH)){
+            return fieldError.getDefaultMessage().concat(String.format(" deve ter entre %s e %s caracteres.",
+                    fieldError.getArguments()[2], fieldError.getArguments()[1]));
+        };
         return fieldError.toString();
     }
 }
